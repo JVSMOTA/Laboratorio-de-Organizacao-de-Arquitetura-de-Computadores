a040a0ecfe14acdc5dda11cbfe2db190a2b743e4
João Vitor Mota <joao.mota@ccc.ufcg.edu.br>
2022-12-15 09:31:16 -0300

    Update - Problema 2: Incompleto

--- Roteiro7/inst2.s
+++ Roteiro7/inst2.s
@@ -1,2 +1,10 @@
 # Problema 2 - Código em Assembly (Leitura e Escrita na Memória de Dados)
 
+.text
+    addi a0, x0, 4             # 4 = Print string
+    la a1, teste               # a1 - endereco que aponta para string
+    ecall                      # Call para saida - string
+
+.data                          # memoria de dados (0x10000000)
+teste:
+    .asciiz "RISC-V!\n"        # string
