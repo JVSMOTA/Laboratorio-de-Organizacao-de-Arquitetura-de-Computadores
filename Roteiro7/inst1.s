8cfda1a6642772d368d528ddbcb588ec5a9cb39c
120111057 JOAO VITOR DE SOUZA MOTA <joao.mota@lcc3-043.lcc.ufcg.edu.br>
2022-12-16 09:09:22 -0300

    Update - Problema 1: Completo

--- Roteiro7/inst1.s
+++ Roteiro7/inst1.s
@@ -2,20 +2,21 @@
 
 .text
 main:
-    addi a0, zero, 4               # Variável N
-    li a4, 1
-    add a4, a4, a4                 # a4 = 2
-    add a1, zero, a0               # aux = N  
+	addi a0, x0, 1                 # 1 - Print Int
+    addi a1, zero, 5               # Variável N
+    li a4, 2                       # a4 = 2
+    add a2, zero, a1               # aux = N  
 loop:
-    beq a1, zero, fimIgualZeroOuUm # if (N == 0) {fim()}
-    beq a1, a4, fim                # if (N == 2) {fim()}
-    addi a1, a1, -1                # aux -= 1
-    mul a3, a0, a1                 # a3 = N * aux
-    add a0, zero, a3               # a0 = a3
+    beq a2, zero, fimIgualZeroOuUm # if (N == 0) {fim()}
+    beq a2, a4, fim                # if (N == 2) {fim()}
+    addi a2, a2, -1                # aux -= 1
+    mul a3, a1, a2                 # a3 = N * aux
+    add a1, zero, a3               # a0 = a3
 	j loop                         # loop()
 
 fimIgualZeroOuUm:
-	addi a0, zero, 1
+	addi a1, zero, 1
     nop
 fim:
     nop
+    ecall
